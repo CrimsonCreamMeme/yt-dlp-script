56c757f3b16ce3023f59d77df74c2eb305e2e122
Crimson Cream <Budde_96@hotmail.com>
2023-05-21 14:18:15 +0300

    Upload file
    Upload file

---
+++ yt-dl.py
@@ -0,0 +1,10 @@
+import os
+thisis=input("Enter URL\n")
+cawk=input("Please chose an option:\n1. Download video/stream.\n2. Download MP3.\n3. Stream download (testing)\n")
+match cawk:
+    case "1":
+        os.system('yt-dlp -f "b" '+thisis)
+    case "2":
+        os.system('yt-dlp -f "bestaudio" -x --audio-format mp3 --audio-quality 0 '+thisis)
+    case "3":
+        os.system('yt-dlp --live-from-start -f "b" '+thisis)
