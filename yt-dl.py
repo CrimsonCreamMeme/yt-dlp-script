c83d0a5a5e98c3840e54bcd556cd4f5cf614cbcc
Crimson Cream <Budde_96@hotmail.com>
2023-05-21 16:44:45 +0300

    Update yt-dl.py
    Update option 3 description.

--- yt-dl.py
+++ yt-dl.py
@@ -1,6 +1,6 @@
 import os
 thisis=input("Enter URL\n")
-cawk=input("Please chose an option:\n1. Download video/stream.\n2. Download MP3.\n3. Stream download (experimental)\n")
+cawk=input("Please chose an option:\n1. Download video/stream.\n2. Download MP3.\n3. Stream download (experimental, will not work on streams has disabled back seeking)\n")
 match cawk:
     case "1":
         os.system('yt-dlp -f "b" '+thisis)
