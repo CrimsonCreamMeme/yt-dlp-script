0e5608b7183046d74bcaa944fdb83279b98be578
Alexander Budde <Budde_96@hotmail.com>
2023-05-31 20:47:21 +0300

    Update yt-dlp.py

--- yt-dlp.py
+++ yt-dlp.py
@@ -1,10 +1,10 @@
 import os
-thisis=input("Enter URL\n")
-cawk=input("Please chose an option:\n1. Download video/stream.\n2. Download MP3.\n3. Stream download (experimental, will not work on streams has disabled back seeking)\n")
-match cawk:
+url=input("Enter URL\n")
+choice=input("Please chose an option:\n1. Download video/stream.\n2. Download MP3.\n3. Stream download (experimental, will not work on streams has disabled back seeking)\n")
+match choice:
     case "1":
-        os.system('yt-dlp -f "b" '+thisis)
+        os.system('yt-dlp -f "b" '+url)
     case "2":
-        os.system('yt-dlp -f "bestaudio" -x --audio-format mp3 --audio-quality 0 '+thisis)
+        os.system('yt-dlp -f "bestaudio" -x --audio-format mp3 --audio-quality 0 '+url)
     case "3":
-        os.system('yt-dlp --live-from-start -f "b" '+thisis)
+        os.system('yt-dlp --live-from-start -f "b" '+url)
