947c28d2cd11e70088772baf9f985674d4d8b033
Alexander Budde <Budde_96@hotmail.com>
2023-06-24 04:17:20 +0300

    Update yt-dlp.py
    Added --embed-metadata.

--- yt-dlp.py
+++ yt-dlp.py
@@ -3,8 +3,8 @@ url=input("Enter URL\n")
 choice=input("Please chose an option:\n1. Download video/stream.\n2. Download MP3.\n3. Stream download (experimental, will not work on streams has disabled back seeking)\n")
 match choice:
     case "1":
-        os.system('yt-dlp -f "b" '+url)
+        os.system('yt-dlp -f "b" --embed-metadata '+url)
     case "2":
-        os.system('yt-dlp -f "bestaudio" -x --audio-format mp3 --audio-quality 0 '+url)
+        os.system('yt-dlp -f "bestaudio" -x --audio-format mp3 --audio-quality 0 --embed-metadata '+url)
     case "3":
-        os.system('yt-dlp --live-from-start -f "b" '+url)
+        os.system('yt-dlp --live-from-start -f "b" --embed-metadata '+url)
